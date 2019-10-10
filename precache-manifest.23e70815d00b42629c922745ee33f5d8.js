edd17db194509c34f7eb1ae2731d7abfcf4c22e8
RomaPux228 <50229125+RomaPux228@users.noreply.github.com>
2019-10-10 15:50:14 +0300

    ,yjdf

---
+++ precache-manifest.23e70815d00b42629c922745ee33f5d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "6fc90a206c06f0d877a0",
+    "url": "/static/js/main.6fc90a20.chunk.js"
+  },
+  {
+    "revision": "3b96c5fcfec5a1381f8c",
+    "url": "/static/js/2.3b96c5fc.chunk.js"
+  },
+  {
+    "revision": "3b96c5fcfec5a1381f8c",
+    "url": "/static/css/2.64694a9b.chunk.css"
+  },
+  {
+    "revision": "85556ece3453250dff706413accd266c",
+    "url": "/index.html"
+  }
+];
